bdee8fea5a10eb67459f67cf94d339ae3539c305
John Kouris <jdkouris@gmail.com>
2019-09-05 18:33:50 -0700

    Initial Commit

---
+++ FindACrew/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FindACrew
+//
+//  Created by John Kouris on 9/5/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
