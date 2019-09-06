44509ff08283a62f24c3be0dc37cee21bb5ad318
John Kouris <jdkouris@gmail.com>
2019-09-05 18:48:56 -0700

    Added groups and necessary code files

---
+++ FindACrew/Models/Person.swift
@@ -0,0 +1,9 @@
+//
+//  Person.swift
+//  FindACrew
+//
+//  Created by John Kouris on 9/5/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import Foundation
---
+++ FindACrew/Views/PersonTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PersonTableViewCell.swift
+//  FindACrew
+//
+//  Created by John Kouris on 9/5/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class PersonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
