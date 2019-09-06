7a55d3d0022eedcfc72be99bcda2d4a3de1554fc
John Kouris <jdkouris@gmail.com>
2019-09-05 19:45:12 -0700

    Added the code for the PersonController to make the network request and get people from the API

--- FindACrew/Model Controllers/PersonController.swift
+++ FindACrew/Model Controllers/PersonController.swift
@@ -7,3 +7,62 @@
 //
 
 import Foundation
+
+class PersonController {
+    
+    private let baseURL = URL(string: "https://swapi.co/api/people")
+    var people = [Person]()
+    
+    enum HTTPMethod: String {
+        case get = "GET"
+        case put = "PUT"
+        case post = "POST"
+        case delete = "DELETE"
+    }
+    
+    func searchForPeople(with searchTerm: String, completion: @escaping () -> Void) {
+        
+        guard let baseURL = baseURL else {
+            completion()
+            return
+        }
+        
+        var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
+        let searchTermQueryItem = URLQueryItem(name: "search", value: searchTerm)
+        urlComponents?.queryItems = [searchTermQueryItem]
+        
+        guard let requestURL = urlComponents?.url else {
+            print("Request URL is nil")
+            completion()
+            return
+        }
+        
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethod.get.rawValue
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                print("Error fetching data: \(error)")
+                return
+            }
+            
+            guard let data = data else {
+                print("No data returned from the data task.")
+                return
+            }
+            
+            let jsonDecoder = JSONDecoder()
+            
+            do {
+                let personSearch = try jsonDecoder.decode(PersonSearch.self, from: data)
+                self.people = personSearch.results
+            } catch {
+                print("Unable to decode data into PersonSearch object: \(error)")
+            }
+            completion()
+            
+        }.resume()
+        
+    }
+    
+}
